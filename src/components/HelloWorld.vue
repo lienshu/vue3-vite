088003b33d549622a6dd1372097ecc86aab2b73d
unknown <1480809371@qq.com>
2022-07-26 18:19:38 +0800

    项目创建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+const value = ref(true);
+// const color=
+</script>
+
+<template>
+	<el-button type="info">+新建项目</el-button>
+	<div class="project">
+		<el-button type="info">体验</el-button>
+		<span class="default">设置默认</span>
+		<el-switch v-model="value" />
+		<el-icon :size="30" :color="hotpink">
+			<Edit />
+		</el-icon>
+	</div>
+</template>
+
+<style scoped>
+.project {
+	margin-top: 20px;
+	border: 2px solid #000;
+	display: flex;
+	padding: 5px 10px 10px 10px;
+	.default {
+		display: line-block;
+		font-size: 12px;
+		margin-top: 2px;
+	}
+}
+</style>
